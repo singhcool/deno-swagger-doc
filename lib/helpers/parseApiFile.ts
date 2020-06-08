24256502866fb15fefd988de0be4943ed1b8e9a6
user <rajasingh.a@contus.in>
2020-06-09 00:26:11 +0530

    import url fixed

--- lib/helpers/parseApiFile.ts
+++ lib/helpers/parseApiFile.ts
@@ -1,6 +1,8 @@
-import { createRequire } from "https://deno.land/std/node/module.ts";
-const require = createRequire(import.meta.url);
-const fs = require("fs");
+// import { createRequire } from "https://deno.land/std/node/module.ts";
+// const require = createRequire(import.meta.url);
+// const fs = require("fs");
+import { readFileStr, readFileStrSync } from "https://deno.land/std/fs/mod.ts";
+
 
 import * as path from "https://deno.land/std/path/mod.ts";
 import { parseApiFileContent } from './parseApiFileContent.ts';
@@ -11,7 +13,8 @@ import { parseApiFileContent } from './parseApiFileContent.ts';
  * @returns {{jsdoc: array, yaml: array}} JSDoc comments and Yaml files
  */
 export function parseApiFile(file : any) {
-  const fileContent = fs.readFileSync(file, { encoding: 'utf8' });
+  const fileContent = readFileStrSync(file, { encoding: 'utf8' });
+  //const fileContent = fs.readFileSync(file, { encoding: 'utf8' });
   const ext = path.extname(file);
 
   return parseApiFileContent(fileContent, ext);
