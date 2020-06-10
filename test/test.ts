e749a58b12326ba7224762220e527ab7e97dbfc3
user <rajasingh.a@contus.in>
2020-06-10 13:26:53 +0530

    swagger doc test

---
+++ test/test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+import { swaggerDoc } from "../mod.ts";
+import { SwaggerJson } from './testData.ts';
+
+const swaggerDefinition = {
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+      description: 'A sample API',
+    },
+    host: `localhost:8000`,
+    basePath: '/',
+  };
+  
+  const options = {
+    swaggerDefinition,
+    apis: ['./example/v2/routes.ts', './example/v2/parameters.yaml'],
+  };
+
+  Deno.test('swaggerDoc()', async () => {
+    const swaggerSpec = swaggerDoc(options);
+    assertEquals(swaggerSpec, JSON.parse(SwaggerJson));
+  });
