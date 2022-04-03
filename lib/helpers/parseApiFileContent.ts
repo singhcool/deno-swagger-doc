5824ae9cc9699412998f6f971356d51892a198de
michaill <zelenac@treska.sk>
2022-04-03 13:42:01 +0200

    cleaner code

--- lib/helpers/parseApiFileContent.ts
+++ lib/helpers/parseApiFileContent.ts
@@ -1,6 +1,6 @@
 import jsYaml from 'https://dev.jspm.io/js-yaml';
 import doctrine from 'https://dev.jspm.io/doctrine';
-
+import { swaggerEnumParser } from './swaggerEnumParser.ts';
 
 /**
  * Parse the provided API file content.
@@ -22,37 +22,9 @@ export function parseApiFileContent(fileContent: any, ext: any) {
     const regexResults = fileContent.match(jsDocRegex);
     if (regexResults) {
       for (let i = 0; i < regexResults.length; i += 1) {
-        if (regexResults[i].indexOf('@swagger-enum') !== -1) {
-          const endOfJsDocComment = fileContent.indexOf(regexResults[i]) + regexResults[i].length;
-          fileContent = fileContent.substring(endOfJsDocComment);
-          const [all, enumName, enumDef] = fileContent.match(/enum\s([^\s]*)\s{([^}]*)}/m);
-          if (enumName && enumDef) {
-            const enumItems = enumDef.split(/[\r\n]+/).filter((i: string) => i.trim() !== '');
-            if (enumItems.length) {
-              let itemsString = '';
-              let type = 'number';
-              enumItems.forEach((item: string) => {
-                if (item.indexOf('=') === -1) {
-                  itemsString += '*         - ' + item.trim().replace(/,$/, '') + '\n';
-                } else {
-                  const itemParts = item.split('=');
-                  if (Number.isNaN(Number(itemParts[1].trim().replace(/,$/, '')))) {
-                    type = 'string';
-                  }
-                  itemsString += ' *         - ' + itemParts[1].trim().replace(/,$/, '') + '\n';
-                }
-              });
-              regexResults[i] = `/**
- * @swagger
- * components:
- *   schemas:
- *     ${enumName}:
- *       type: ${type}
- *       enum:
-${itemsString} */`;
-            }
-          }
-        }
+        const { content, jsDoc } = swaggerEnumParser(fileContent, regexResults[i]);
+        fileContent = content;
+        regexResults[i] = jsDoc;
         const jsDocComment = doctrine.parse(regexResults[i], { unwrap: true });
         jsDocComments.push(jsDocComment);
       }
